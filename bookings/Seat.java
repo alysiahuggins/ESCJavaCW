f805ecf8661f3b16538867c7af89b99e18594822
soukaina <nhsoukaina@gmail.com>
2014-02-03 15:21:51 +0000

    13 warnings left

--- bookings/Seat.java
+++ bookings/Seat.java
@@ -9,16 +9,28 @@ public class Seat {
 
     private final char row;
     private final int number;
-    
+
+    //@ invariant row>= MIN_ROW & row <=MAX_ROW
+    //@ invariant MIN_NUMBER<=number & number<=MAX_NUMBER
+
+    //@ requires row>= MIN_ROW & row <=MAX_ROW
+    //@ requires MIN_NUMBER<=number & number<=MAX_NUMBER
+    //@ ensures this.row>= MIN_ROW & this.row <=MAX_ROW
+    //@ ensures MIN_NUMBER<=this.number & this.number<=MAX_NUMBER
     public Seat(char row, int number) {
         this.row = row;
         this.number = number;
     }
 
+    //@ requires row>= MIN_ROW & row <=MAX_ROW
+
+    //@ ensures \result>= MIN_ROW & \result <=MAX_ROW
     public final char getRow() {
         return row;
     }
 
+    //@ requires MIN_NUMBER<=number & number<=MAX_NUMBER
+    //@ ensures MIN_NUMBER<=\result & \result<=MAX_NUMBER
     public final int getNumber() {
         return number;
     }
--- bookings/SeatReservationManager.java
+++ bookings/SeatReservationManager.java
@@ -2,62 +2,82 @@ package bookings;
 
 public class SeatReservationManager {
 
-    private final Customer[][] seatReservations;
-    
+    private final /*non_null*/Customer[][] seatReservations;
+
+
     public SeatReservationManager() {
         seatReservations = new Customer[rowToIndex(Seat.MAX_ROW) + 1]
-                                       [numberToIndex(Seat.MAX_NUMBER) + 1];
+                [numberToIndex(Seat.MAX_NUMBER) + 1];
     }
 
-    public boolean isReserved(Seat s) {
+    //@ requires \nonnullelements(seatReservations);
+
+
+    public boolean isReserved(/*@ non_null */Seat s) {
         return seatReservations[rowToIndex(s.getRow())]
-                               [numberToIndex(s.getNumber())] != null;
+                [numberToIndex(s.getNumber())] != null;
     }
 
-    public void reserve(Seat s, Customer c) 
+
+    //@ requires \nonnullelements(seatReservations);
+    //@ requires \typeof(c)<: \elemtype(\typeof(seatReservations));
+    public void reserve(/*@ non_null*/Seat  s, /*@ non_null*/Customer c)
             throws ReservationException {
         if(isReserved(s)) {
             throw new ReservationException();
         }
         seatReservations[rowToIndex(s.getRow())]
-                        [numberToIndex(s.getNumber())] = c;
+                [numberToIndex(s.getNumber())] = c;
     }
-    
-    public void unreserve(Seat s)
+
+    //@ requires \nonnullelements(seatReservations);
+    public void unreserve(/*@ non_null*/Seat s)
             throws ReservationException {
         if(!isReserved(s)) {
             throw new ReservationException();
         }
         seatReservations[rowToIndex(s.getRow())]
-                        [numberToIndex(s.getNumber())] = null;
+                [numberToIndex(s.getNumber())] = null;
     }
 
-    public void reserveNextFree(Customer c) throws ReservationException {
+    //@ requires \nonnullelements(seatReservations);
+    //@ requires \typeof(c)<: \elemtype(\typeof(seatReservations));
+    //@ requires c!=null
+
+    public void reserveNextFree(/* non_null */Customer c) throws ReservationException {
         for(int rowIndex = 0; rowIndex < seatReservations.length; rowIndex++) {
-            for(int numberIndex = 0; 
-                    numberIndex < seatReservations[rowIndex].length; 
-                    numberIndex++) {
-                Seat current = new Seat(indexToRow(rowIndex), 
-                    indexToNumber(numberIndex));
+            for(int numberIndex = 0;
+                numberIndex < seatReservations[rowIndex].length;
+                numberIndex++) {
+                Seat current = new Seat(indexToRow(rowIndex),
+                        indexToNumber(numberIndex));
                 if(!isReserved(current)) {
                     reserve(current, c);
                     return;
                 }
             }
         }
+
         throw new ReservationException();
-    }
+    }//@ nowarn Exception
+
     
     /*@ ghost String toStringResult; in privateState;
         represents theString <- toStringResult;
-    @*/
- 
+@*/
+
+
+    //@ requires \nonnullelements(seatReservations)
+    //@ invariant (\forall int i; i>=0 && i<= seatReservations.length ==> \nonnullelements(seatReservations[i]))
+
     public String toString() {
 
         String result = " ";
-        
-        for(int numberIndex = 0; numberIndex < seatReservations[0].length; 
-                numberIndex++) {
+
+
+        for(int numberIndex = 0; numberIndex < seatReservations[0].length;
+            numberIndex++) {
+
             result += " " + indexToNumber(numberIndex);
         }
         result += "\n";
@@ -66,35 +86,42 @@ public class SeatReservationManager {
             rowIndex < seatReservations.length;
             rowIndex++) {
             result += indexToRow(rowIndex);
-            for(int numberIndex = 0; numberIndex < 
+            for(int numberIndex = 0; numberIndex <
                     seatReservations[rowIndex].length; numberIndex++) {
                 for(int j = numberIndex; j >= 10; j /= 10) {
                     result += " ";
                 }
-                result += " " + (isReserved(new Seat(indexToRow(rowIndex), 
-                    indexToNumber(numberIndex))) ? "X" : " ");
+
+                result += " " + (isReserved(new Seat(indexToRow(rowIndex),
+                        indexToNumber(numberIndex))) ? "X" : " ");
             }
             result += "\n";
         }
-        
+
         //@ set toStringResult = result;
         return result;
     }
-
+    //@ requires row>=Seat.MIN_ROW & row<=Seat.MAX_ROW
+    //@ ensures \result>=0 & \result<= Seat.MAX_ROW-Seat.MIN_ROW
     private static int rowToIndex(char row) {
         return row - Seat.MIN_ROW;
     }
-
+    //@ requires number>=Seat.MIN_NUMBER & number<=Seat.MAX_NUMBER
+    //@ ensures \result>=0 & \result<= Seat.MAX_NUMBER-Seat.MIN_NUMBER
     private static int numberToIndex(int number) {
         return number - Seat.MIN_NUMBER;
     }
-    
+
+    //@ requires index>=0 & index<Seat.MAX_ROW-Seat.MIN_ROW;
+    //@ ensures \result>=Seat.MIN_ROW & \result<=Seat.MAX_ROW
     private static char indexToRow(int index) {
         return (char)(Seat.MIN_ROW + index);
     }
 
+    //@ requires index>=0 & index<Seat.MAX_NUMBER-Seat.MIN_NUMBER;
+    //@ ensures Seat.MIN_NUMBER<=\result & \result<=Seat.MAX_NUMBER
     private static int indexToNumber(int index) {
         return index + Seat.MIN_NUMBER;
     }
-    
+
 }
