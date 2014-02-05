b7eaa4fe0c2c3a271340400bbcc07c92eef8fd7e
nhsoukai <nhsoukaina@gmail.com>
2014-02-05 16:53:46 +0000

    demo

--- bookings/SeatReservationDemo.java
+++ bookings/SeatReservationDemo.java
@@ -3,7 +3,30 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
-        
+
+        SeatReservationManager manager= new SeatReservationManager();
+        Customer samantha= new Customer();
+        Customer alysia= new Customer();
+        Customer soukaina= new Customer();
+        Customer sue= new Customer();
+        Seat souSeat= new Seat('A', 1); // add min value for row and number
+        Seat samSeat= new Seat('A', 11);
+        Seat alSeat= new Seat('B', 10);
+        Seat sueSeat= new Seat('G', 20); // add max value for row and number
+        manager.reserve(samSeat,samantha);
+        manager.reserve(souSeat,soukaina);
+        manager.reserve(sueSeat,alysia);
+
+        int i=0;
+        while(i<42){
+
+            manager.reserveNextFree(sue);
+            i++;
+        }
+
+
+        System.out.println(manager);
+
     }
-    
+
 }
